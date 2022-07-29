074b6e4b8dd807b49f78db975a96592fac362d0d
JoaoScheleder <jg.scheleder@hotmail.com>
2022-07-29 13:14:19 -0300

    models_and_migrations
    criado os modelos e as tabelas do banco de dados.

---
+++ database/migrations/2022_07_29_154811_create_pacientes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePacientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pacientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('cpf',11)->unique();
+            $table->timestamp('data_hora_cadastro');
+            $table->string('telefone',13);
+            $table->string('email',254);
+            $table->string('cep',8);
+            $table->text('endereço');
+            $table->integer('numero');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pacientes');
+    }
+}
--- database/migrations/2022_07_29_154829_create_consultas_table.php
+++ database/migrations/2022_07_29_154829_create_consultas_table.php
@@ -1,10 +1,11 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateConsultasTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +14,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('consultas', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignIdFor(User::class,'paciente_id');
+            $table->foreignIdFor(User::class,'medico_id');
+            $table->timestamp('data_hora_consulta');
             $table->timestamps();
         });
     }
@@ -31,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('consultas');
     }
 }
